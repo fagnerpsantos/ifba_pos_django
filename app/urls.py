6146cf47c895a7fc0b865c8421d74d4c54a25387
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2019-11-21 15:24:46 -0300

    Autenticação pronta

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('cadastrar_chave', cadastrar_chave, name='cadastrar_chave'),
     path('editar_chave/<int:id>', editar_chave, name='editar_chave'),
     path('remover_chave/<int:id>', remover_chave, name='remover_chave'),
+    path('cadastrar_usuario', cadastrar_usuario, name='cadastrar_usuario'),
+    path('logar_usuario', logar_usuario, name='logar_usuario'),
+    path('deslogar_usuario', deslogar_usuario, name='deslogar_usuario'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from .models import Chave
 from .forms import ChaveForm
@@ -8,6 +11,7 @@ def listar_chaves(request):
     chaves = Chave.objects.all()
     return render(request, 'chaves/index.html', {'chaves': chaves})
 
+@login_required()
 def cadastrar_chave(request):
     if request.method == "POST":
         form_chave = ChaveForm(request.POST)
@@ -18,6 +22,7 @@ def cadastrar_chave(request):
         form_chave = ChaveForm()
     return render(request, 'chaves/form_chave.html', {'form_chave': form_chave})
 
+@login_required()
 def editar_chave(request, id):
     chave_bd = Chave.objects.get(id=id)
     form_chave = ChaveForm(request.POST or None, instance=chave_bd)
@@ -27,9 +32,41 @@ def editar_chave(request, id):
         return redirect('listar_chaves')
     return render(request, 'chaves/form_chave.html', {'form_chave': form_chave})
 
+@login_required()
 def remover_chave(request, id):
     chave = Chave.objects.get(id=id)
     if request.method == "POST":
         chave.delete()
         return redirect('listar_chaves')
     return render(request, 'chaves/confirma_exclusao.html', {'chave': chave})
+
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('listar_chaves')
+    else:
+        form_usuario = UserCreationForm()
+    return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+
+def logar_usuario(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        usuario = authenticate(request, username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('listar_chaves')
+        else:
+            form_login = AuthenticationForm()
+    else:
+        form_login = AuthenticationForm()
+    return render(request, 'usuarios/login.html', {'form_login': form_login})
+
+def deslogar_usuario(request):
+    logout(request)
+
+    return redirect('listar_chaves')
